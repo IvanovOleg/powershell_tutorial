6622916b013da5fa87e0355d2e0b437f848a93b6
Oleg Ivanov <oleg.ivanov@globallogic.com>
2016-12-12 16:49:30 +0200

    Added a REST API functions.

--- lesson5.ps1
+++ lesson5.ps1
@@ -41,7 +41,7 @@ $connectionParameters = @(
 $Env:yourPassword = "qwerty" # имитируем наличие пееменной окружения
 $connectionString = $connectionParameters -join ';' # объеденим элементы массива в строку
 $newPassword = ConvertTo-SecureString $Env:yourPassword -AsPlainText -Force # поместим пароль в строку в зашифрованом виде
-
+$externalip = ((Invoke-WebRequest http://myexternalip.com/raw).Content).trim() # получить внешний ip с помошью веб-запроса
 
 #
 # Functions
@@ -64,7 +64,30 @@ function DeleteFile ($path) {
     Write-Output "The ${path} file has been deleted"
 }
 
+function GetFakeResource {
+    $uri = "http://jsonplaceholder.typicode.com/posts/1"
+    $header = @{
+        "Content-Type" = "application/json"
+    }
+    $result = Invoke-RestMethod -Uri $uri -Method "GET" -Headers $header
+    return $result
+}
 
+function SetFakeResource {
+    $uri = "http://jsonplaceholder.typicode.com/posts"
+    $header = @{
+        "Content-Type" = "application/json"
+    }
+    $body = @{
+        "data" = @{
+            "title" = "foo"
+            "body" = "bar"
+            "userId" = 1
+        }
+    } | ConvertTo-Json
+    $result = Invoke-RestMethod -Uri $uri -Method "POST" -Headers $header -Body $body
+    return $result
+}
 
 #
 # Execution
@@ -74,6 +97,7 @@ $Env:yourPassword
 $connectionString
 $newPassword
 
+# -foreground позволяет выделить текст другим цветом
 Write-Host "`n[ Creating a powershell credential ]`n" -foreground "green"
 $psCredential = CreatePowershellCredential -username $firstName -password $newPassword
 $psCredential.GetType()
@@ -84,3 +108,18 @@ $decodedPassword
 
 Write-Host "`n[ Deleting a file ]`n" -foreground "green"
 DeleteFile -path ".\delete.me"
+
+Write-Host "`n[ Getting a fake resource using a REST API call ]`n" -foreground "green"
+$response = (GetFakeResource)
+$response
+
+Write-Host "`n[ Creating a fake resource using a REST API call ]`n" -foreground "green"
+$response = (SetFakeResource)
+$response
+
+
+# Домашнее задание
+
+# http://jsonplaceholder.typicode.com/
+# https://github.com/PoshCode/PowerShellPracticeAndStyle
+# https://en.wikipedia.org/wiki/Don't_repeat_yourself
